7f0e89a1a680657676ab632f8f4974e82ed47709
mjg54 <guertinmj@gmail.com>
2021-08-21 16:02:43 -0400

    Add files via upload

---
+++ pause_index.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+Args=commandArgs(TRUE)
+argsLen <- length(Args)
+
+densities = read.table(Args[1], header =FALSE)
+prefix.name = strsplit(Args[1], "_pause_body.bed")[[1]][1]
+median(densities[,10] )
+
+pdf(paste(prefix.name,'_pause_index.pdf', sep=''), width=2.5, height=4, useDingbats=FALSE)
+par(pty="s")
+boxplot(log(densities[,10], base = 10), main = paste('median = ', round(median(densities[,10] ), 2)),
+        pch = 16, outline=FALSE)
+abline(h = log(10, base = 10), col='blue', lty = 2)
+dev.off()
+
