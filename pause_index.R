07e9998431eeb082869959318083660e35738812
mjg54 <guertinmj@gmail.com>
2021-08-21 17:10:22 -0400

    Update pause_index.R

--- pause_index.R
+++ pause_index.R
@@ -10,7 +10,7 @@ median(densities[,10] )
 pdf(paste(prefix.name,'_pause_index.pdf', sep=''), width=2.5, height=4, useDingbats=FALSE)
 par(pty="s")
 boxplot(log(densities[,10], base = 10), main = paste('median = ', round(median(densities[,10] ), 2)),
-        pch = 16, outline=FALSE)
+        pch = 16, outline=FALSE, ylab=expression("log"[10]~"(Pause Index)"))
 abline(h = log(10, base = 10), col='blue', lty = 2)
 dev.off()
 
