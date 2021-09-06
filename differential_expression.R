2c62d4d70bd160ebc0a56cafd25caff92661b91d
mjg54 <guertinmj@gmail.com>
2021-09-06 11:20:45 -0400

    Update differential_expression.R

--- differential_expression.R
+++ differential_expression.R
@@ -26,6 +26,9 @@ rownames(x) = x[,1]
 
 x = x[,seq(2,to=ncol(x),by=2)]
 
+colnames(x) = colnames(x)[,c(grep(Args[2], colnames(x)), c(1:length(colnames(x)))[!(c(1:length(colnames(x))) %in% grep(Args[2], colnames(x)))])]
+
+
 sample.conditions = factor(c(rep("untreated",n.untreated), rep("treated",n.treated)),
                            levels=c("untreated","treated"))
 
