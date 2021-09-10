81570e0e82d8c0707c5e7b46df4d394c8189637b
mjg54 <guertinmj@gmail.com>
2021-09-10 13:08:23 -0400

    Update render_with_pandoc.sh

--- render_with_pandoc.sh
+++ render_with_pandoc.sh
@@ -1,1 +1,3 @@
+wget https://raw.githubusercontent.com/guertinlab/Nascent_RNA_Methods/main/README.md
+
 pandoc --filter pandoc-citeproc --bibliography=nascent_rna_methods.bib -s README.md -o Methods_Ms.pdf
