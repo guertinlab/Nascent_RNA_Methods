d7e7384b8b56f778c8827ef86a2c3b7190f4f461
mjg54 <guertinmj@gmail.com>
2021-09-14 15:08:09 -0400

    Update differential_expression.R

--- differential_expression.R
+++ differential_expression.R
@@ -9,7 +9,7 @@ plot.ma.lattice <- function(ma.df, filename = 'file.name', p = 0.01, title.main 
   pdf(paste("MA_plot_", filename, ".pdf", sep=''), width=3.83, height=3.83)
   print(xyplot(ma.df$log2FoldChange ~ log(ma.df$baseMean, base=10),
                groups=(ma.df$padj < p & abs(ma.df$log2FoldChange) > log2fold & !is.na(ma.df$padj)),
-               col=c("grey40","red"), main=title.main, scales="free", aspect=1, pch=20, cex=0.5,
+               col=c("grey40","red"), main=title.main, scales="free", aspect=1, pch=20, cex=0.25,
                ylab=expression("log"[2]~"PRO fold change"), xlab=expression("log"[10]~"Mean of Normalized Counts"),
                par.settings=list(par.xlab.text=list(cex=1.1,font=2), par.ylab.text=list(cex=1.1,font=2))))
   dev.off()
