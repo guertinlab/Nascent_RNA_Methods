240b05865097176a88fdd855cac715318c2b3c41
mjg54 <guertinmj@gmail.com>
2021-09-16 13:14:31 -0400

    Update render_with_pandoc.sh

--- render_with_pandoc.sh
+++ render_with_pandoc.sh
@@ -2,4 +2,4 @@ wget https://raw.githubusercontent.com/guertinlab/Nascent_RNA_Methods/main/READM
 wget https://raw.githubusercontent.com/guertinlab/Nascent_RNA_Methods/main/nascent_rna_methods.bib
 #adapted from https://raw.githubusercontent.com/citation-style-language/styles/master/springer-basic-brackets.csl
 wget https://raw.githubusercontent.com/guertinlab/Nascent_RNA_Methods/main/springer-basic-parentheses-bold-italics.csl
-pandoc --number-sections --citeproc  --bibliography nascent_rna_methods.bib -s README.md -o Methods_Ms.pdf --csl springer-basic-brackets.csl
+pandoc --number-sections --citeproc --bibliography nascent_rna_methods.bib -s README.md -o Methods_Ms.pdf --csl springer-basic-parentheses-bold-italics.csl
