21063196e1dccf2e0e7f25cac5aab109fb67646f
guertinlab <guertin@virginia.edu>
2021-09-15 20:10:26 -0400

    Update render_with_pandoc.sh

--- render_with_pandoc.sh
+++ render_with_pandoc.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/guertinlab/Nascent_RNA_Methods/main/README.md
 wget https://raw.githubusercontent.com/guertinlab/Nascent_RNA_Methods/main/nascent_rna_methods.bib
-pandoc --citeproc --number-sections --bibliography=nascent_rna_methods.bib -s README.md -o Methods_Ms.pdf
-#pandoc --biblatex --number-sections --bibliography=nascent_rna_methods.bib -s README.md -o Methods_Ms.pdf
+#pandoc --citeproc --number-sections --bibliography=nascent_rna_methods.bib -s README.md -o Methods_Ms.pdf
+pandoc --number-sections --bibliography=nascent_rna_methods.bib -s README.md -o Methods_Ms.pdf
